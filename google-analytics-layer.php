7431c543d0bd8a8914f99f822fdf5f9727b1a057
kufeiko <kufeiko@gmail.com>
2012-06-25 17:14:28 +0300

    Fixed variable checking error.

--- google-analytics-layer.php
+++ google-analytics-layer.php
@@ -7,7 +7,7 @@ function head_script() {// insert Javascript into the <head>
 		$google_UA = qa_opt('google_analytics_UA');
 		$is_admin = (qa_get_logged_in_level() == 120) ? true : false;
 
-    if (isset($google_UA)) { 
+    if (!empty($google_UA)) { 
 			if (!($is_admin && qa_opt('google_analytics_show_for_admin'))) { // the loged in user is not the admin
 				$this->content['script'][]='<script type="text/javascript">'.
 				'var _gaq = _gaq || [];'.
