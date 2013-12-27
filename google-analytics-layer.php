0e09f3cb0d4355918b9ba7f44a206f534ecb916c
tanaydin <huzursuz@gmail.com>
2013-12-28 00:00:17 +0200

    added support for setDomainName option of analytics.

--- google-analytics-layer.php
+++ google-analytics-layer.php
@@ -3,16 +3,24 @@
 class qa_html_theme_layer extends qa_html_theme_base {
 
 function head_script() {// insert Javascript into the <head>
-		
-		$google_UA = qa_opt('google_analytics_UA');
+
+        $google_UA = qa_opt('google_analytics_UA');
+        $google_domain = qa_opt('google_analytics_domain');
 		$is_admin = (qa_get_logged_in_level() == 120) ? true : false;
 
-    if (!empty($google_UA)) { 
+    if (!empty($google_UA)) {
+
+        $set_domain = '';
+        if (!empty($google_domain)) {
+            $set_domain = '_gaq.push([\'_setDomainName\', \''.$google_domain.'\']);';
+        }
+
 			if (!($is_admin && qa_opt('google_analytics_show_for_admin'))) { // the loged in user is not the admin
 				$this->content['script'][]='<script type="text/javascript">'.
 				'var _gaq = _gaq || [];'.
         '_gaq.push([\'_setAccount\', \''.$google_UA.'\']);'.
         '_gaq.push([\'_trackPageview\']);'.
+        $set_domain.
         '(function() {'.
         'var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;'.
         'ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';'.
